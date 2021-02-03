fe664218f2c4eadf1f2d2236d19e5dbc523c809d
e-floran <e-floran@hotmail.fr>
2021-02-03 16:25:51 +0100

    ajout du router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
+
 import "./styles/header.css";
+
 export default function Header(){
     return(
         <header>
             <img src="../../foulpougne-logo.jpg" alt="Foulpougne festival" className="logoImg"/>
             <nav>
                 <ul>
-                    <li>Résumés</li>
-                    <li>Connexion</li>
+                    <Link to="/" title="Résumés de nos éditions précédentes">
+                        <li>Résumés</li>
+                    </Link>
+                    <Link to="/login/" title="Connexion administrateur">
+                        <li>Connexion</li>
+                    </Link>
                 </ul>
             </nav>
         </header>
