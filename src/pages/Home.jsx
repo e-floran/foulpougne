6ddb856b86c58b49bed0a0f55c6ab721f477288f
Floran <e-floran@hotmail.fr>
2023-07-23 16:41:42 +0200

    added picture of the year

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ export default function Home() {
       <section className="homeContent">
         <h2>La prochaine édition du festival aura lieu</h2>
         <h2> du 22 au 24 septembre 2023</h2>
+        <article className="homePictureContainer">
+          <img src="/pictures/affiche_2023.png" alt="affiche 2023 du festival Foulpougne" />
+        </article>
         {/* <div className="homeDetailsContainer">
           <article>
             <h3>Vendredi 23</h3>
