c484b7c98c2ce781704f605fe3e6eb889d81a76c
Floran <70599734+e-floran@users.noreply.github.com>
2025-06-15 13:35:06 +0200

    Added new site link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <div className="pageContainer">
       <section className="homeContent">
-        <h2>La prochaine édition du festival aura lieu</h2>
+        <h2>Veuillez désormais retrouver les informations sur le festival sur le site suivant : <a href="foulpougnefestival.wixsite.com/foulpougne-festival">nouveau site Foupougne festival</a></h2>
+        {/* <h2>La prochaine édition du festival aura lieu</h2>
         <h2> du 20 au 22 septembre 2024</h2>
         <article className="homePictureContainer">
           <img src="/pictures/affiche_2024.png" alt="affiche 2024 du festival Foulpougne" />
-        </article>
+        </article> */}
         {/* <div className="homeDetailsContainer">
           <article>
             <h3>Vendredi 23</h3>
